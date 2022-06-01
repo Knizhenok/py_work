e883a9440ca2ab99af05acd8fa2eba9afd665ef1
Denis <d.engineer@tut.by>
2022-06-01 15:43:58 +0300

    01.06V1

--- lern/oop/ex1_ClassObject.py
+++ lern/oop/ex1_ClassObject.py
@@ -6,6 +6,7 @@ class Point:
     color = 'red'
     circle = 2
 
+
 # Point.__dict__ вывести все по классу
 # type(object) выведет родительский класс
 
@@ -14,15 +15,34 @@ class Point:
 Point.type_pt = 'disk'
 setattr(Point, 'prop', 1)
 
-
 #  считать если атрибут не существует и для избежания ошибки кроме
 #  надо использовать такой запрос с подменой
 getattr(Point, 'a', False)
 
 # удаление атрибута
-del Point.type_pt
-delattr(Point, 'type_pt')
+# del Point.type_pt
+# delattr(Point, 'type_pt')
 
 # проверка наличия атрибута в классе
 hasattr(Point, 'prop')
+
+
+if __name__ == '__main__':
+    a = Point()
+    # print(getattr(a, 'col', False))
+    Point.circle += 1
+    a.start_pt = (10, 5)
+    a.end_pt = (100, 20)
+    a.color = 'blue'
+    del a.color
+
+    # print(getattr(a, 'start_pt', False))
+
+    if 'color' in a.__dict__.keys():
+        print(True)
+    else:
+        print(False)
+
+    # print(*a.__dict__.keys())
+
 
